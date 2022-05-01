ee4c11698bd1c866fbd9c905afd698e035a0e7a0
ahmed kadry <ahmedyoussefkadry95@gmail.com>
2022-05-01 19:36:38 +0200

    ios login with google

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,6 @@ void main() async {
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
